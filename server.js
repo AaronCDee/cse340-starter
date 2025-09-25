79d06a6f3e6781155e1d8c44650c56eef872b57b
Aaron Delate <aaron@zero-one.io>
2025-09-25 18:14:10 +0200

    crimesjohnson:feat: add registration and login view

--- server.js
+++ server.js
@@ -12,10 +12,12 @@ const static = require("./routes/static")
 const expressLayouts = require("express-ejs-layouts")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const accountRoute = require("./routes/accountRoute")
 const errRoute       = require("./routes/errorRoute")
 const utilities = require("./utilities")
 const session = require("express-session")
 const pool = require('./database/')
+const bodyParser = require("body-parser")
 
 /* ***********************
  * Middleware
@@ -30,6 +32,9 @@ const pool = require('./database/')
   saveUninitialized: true,
   name: 'sessionId',
 }))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 // Express Messages Middleware
 app.use(require('connect-flash')())
@@ -55,6 +60,7 @@ app.get('/', utilities.handleErrors(baseController.buildHome))
 
 // Inventory routes
 app.use("/inv", utilities.handleErrors(inventoryRoute))
+app.use("/account", utilities.handleErrors(accountRoute))
 app.use("/error", utilities.handleErrors(errRoute))
 
 // File Not Found Route - must be last route in list
